58eb37149d300f14575aad837efb2a7cfb62343c
Gonzalo De Domingo <gonzalodedomingo@gmail.com>
2023-02-17 16:03:51 -0300

    Fix styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -6,6 +7,11 @@ module.exports = {
       colors: {
         'custom-color-1': '#18293c',
       },
+    },
+    screens: {
+      xs: '100px',
+      md: '720px',
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
