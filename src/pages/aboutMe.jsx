d3a1c33ef1e54de8e4773e5e35169c9d7820bd8e
Gonzalo De Domingo <gonzalodedomingo@gmail.com>
2023-02-17 13:35:58 -0300

    first commit

---
+++ src/pages/aboutMe.jsx
@@ -0,0 +1,47 @@
+import { useTranslation } from 'react-i18next'
+
+import ProfilePic from '../assets/profile-pic-noback.png'
+import Tabs from '../components/tabs'
+import { skills, experience, education } from '../assets/data.json'
+
+const AboutMe = () => {
+  const { t } = useTranslation('ABOUT_ME')
+
+  return (
+    <div id="glassCard" className="flex justify-start flex-col items-center">
+      <h1 className="mt-10 text-3xl font-medium mb-10 md:hidden">
+        {t('TITLE')}
+      </h1>
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="flex flex-[0.5] justify-center">
+          <div className="glassCard rounded-lg mx-5 h-max md:mt-[110px] max-w-[70%] md:max-w-[70%]">
+            <img src={ProfilePic} className="rounded-lg" />
+          </div>
+        </div>
+        <div className="flex-1 px-5 pt-5 md:pt-0 text-xs">
+          <h1 className="mt-10 text-3xl font-medium mb-10 hidden md:block">
+            {t('TITLE')}
+          </h1>
+          <p className="font-light">{t('DESCRIPTION_1')}</p>
+          <p className="font-light">{t('DESCRIPTION_2')}</p>
+          <div className="mt-5">
+            <p className="font-normal text-base mb-4">Skills</p>
+            {skills.map((skill, i) => (
+              <button
+                key={`${skill}${i}`}
+                className="glassButton py-1 px-3 mr-2 mb-2 font-light text-xs text-custom-color-1"
+              >
+                {skill}
+              </button>
+            ))}
+          </div>
+          <div className="pt-5">
+            <Tabs experience={experience} education={education} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
