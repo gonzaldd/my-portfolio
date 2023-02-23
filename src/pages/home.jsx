2fdc9be6eab1b46785aa3f60e60a9b2c6e6478c0
Gonza <gdedomingo>
2023-02-23 16:15:25 -0300

    Improve styles & toTop on change language

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,9 +6,9 @@ function Home() {
   return (
     <section
       id="home"
-      className="flex flex-1 items-center h-[100vh] md:h-[90vh]"
+      className="flex flex-1 items-center h-[100vh] md:h-[90vh] px-8 py-6 md:px-16 md:py-10"
     >
-      <div className="flex flex-1 flex-col px-8 py-6 md:px-16 md:py-10">
+      <div className="flex flex-1 flex-col">
         <p className="sm:text-2xl font-light mb-2">{t('GREETING')}</p>
         <p className="text-5xl sm:text-6xl lg:text-7xl font-medium">
           {t('MY_NAME')}
