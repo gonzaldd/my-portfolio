66d66c4c3e7bb263d88d16714e21ca143cf6c775
Gonzalo De Domingo <gonzalodedomingo@gmail.com>
2023-02-23 15:57:31 -0300

    Ready for prod

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Header from './components/header'
 import AboutMe from './pages/aboutMe'
 import Portfolio from './pages/portfolio'
 import Footer from './components/footer'
+import Contact from './pages/contact'
 import ErrorPage from './pages/notFound'
 
 function App() {
   return (
-    <div className="fade-in-animation xl:max-w-[80%]">
+    <div className="fade-in-animation xl:max-w-[65%]">
       <Header />
       <Home />
       <AboutMe />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Contact />
       <Footer />
     </div>
