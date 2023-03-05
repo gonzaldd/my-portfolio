349d1fda02f518c1978e15e9dd3ffbb32f4ac93c
Gonzalo <gonzalodedomingo@gmail.com>
2023-03-05 20:26:23 -0300

    Add typescript

--- src/components/basicData.tsx
+++ src/components/basicData.tsx
@@ -1,17 +1,30 @@
 import { useTranslation } from 'react-i18next'
 
-import parseLng from '../utils/parse-lng'
+type Position_Lang = "position" | "position_en"
+type Description_Lang = 'description' | "description_en"
+
+type Row = {
+  company: String,
+  name: String,
+  position: string,
+  position_en: string,
+  time: string,
+  description: string,
+  description_en: string
+}
+
+import parseLng from '../utils/parse-lng.js'
 
 const BasicData = ({ data = [] }) => {
   const { i18n } = useTranslation()
 
   const lng = parseLng(i18n.language)
-  const lngKey = lng === 'es' ? 'description' : 'description_en'
-  const positionKey = lng === 'es' ? 'position' : 'position_en'
+  const lngKey: Description_Lang = lng === 'es' ? 'description' : 'description_en'
+  const positionKey: Position_Lang = lng === 'es' ? 'position' : 'position_en'
 
   return (
     <div className="flex flex-wrap w-full gap-y-12">
-      {data.map((rowData, i) => (
+      {data.map((rowData: Row, i) => (
         <div key={`${rowData?.company}${i}`} className={`w-full`}>
           <div className="flex flex-col-reverse md:flex-row gap-x-5 items-baseline">
             <p className="font-medium text-xl xl:text-2xl mb-4">
---
+++ src/utils/parse-lng.ts
@@ -0,0 +1,1 @@
+export default (i18nLng: string): string => (/^es(-.*)?$/.test(i18nLng) ? 'es' : 'en')
